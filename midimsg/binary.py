e5c7b1f7524163f929fa6728b7ea10a6dc68f0a5
Ole Martin Bjørndalen <ombdalen@gmail.com>
2018-12-17 18:55:35 +0100

    Completed decoders.

--- midimsg/binary.py
+++ midimsg/binary.py
@@ -1,4 +1,3 @@
-# TODO: add some safety?
 from .messages import *
 
 
@@ -9,6 +8,7 @@ def ch(m):
 encoders = {
     'NoteOff': lambda msg: (0x80|msg.ch-1, msg.note, msg.velocity),
     'NoteOn': lambda msg: (0x90|msg.ch-1, msg.note, msg.velocity),
+
     'ControlChange': lambda msg: (0xb0|msg.ch-1, msg.number, msg.value),
     'SystemExclusive': lambda msg: (0xf0,) + tuple(msg.data) + (0xf7,),
     'Reset': lambda msg: (0xff,)
@@ -16,11 +16,24 @@ encoders = {
 
 
 decoders = {
-    0x80: lambda m: NoteOff(ch(m), m[1], m[2]),
-    0x90: lambda m: NoteOn(ch(m), m[1], m[2]),
-    0xb0: lambda m: ControlChange(ch(m), m[1], m[2]),
-    0xf0: lambda m: SystemExclusive(bytes(m[1:-1])),
-    0xff: lambda m: Reset()
+    0x80: lambda Q: NoteOff(ch(B), Q[1], Q[2]),
+    0x90: lambda Q: NoteOn(ch(B), Q[1], Q[2]),
+    0xa0: lambda Q: PolyPressure(ch(B), Q[1], Q[2]),
+    0xb0: lambda Q: ControlChange(ch(B), Q[1], Q[2]),
+    0xc0: lambda Q: ProgramChange(ch(B), Q[1]),
+    0xd0: lambda Q: ChannelPressure(ch(B), Q[1]),
+    0xe0: lambda Q: PitchBend(ch(B), (Q[1] | Q[2] << 7)),  # ?
+    0xf0: lambda Q: SystemExclusive(bytes(Q)),
+    0xf1: lambda Q: TimeCode(Q[1] >> 4, Q[1] & 15),  # ?
+    0xf2: lambda Q: SongPosition((Q[1] | Q[2] << 7)),  # ?
+    0xf3: lambda Q: SongSelect(Q[1]),
+    0xf6: lambda Q: TuneRequest(),
+    0xf8: lambda Q: MidiClock(),
+    0xfa: lambda Q: Start(),
+    0xfb: lambda Q: Continue(),
+    0xfc: lambda Q: Stop(),
+    0xfe: lambda Q: ActiveSensing(),
+    0xff: lambda Q: Reset(),
 }
 
 
