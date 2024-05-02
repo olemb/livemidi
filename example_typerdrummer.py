5333952e420e058175716224081bc9aa57e57a90
Ole Martin Bjørndalen <ombdalen@gmail.com>
2024-05-02 08:07:08 +0200

    Added example typerdrummer. Not quite working yet.

---
+++ example_typerdrummer.py
@@ -0,0 +1,51 @@
+"""
+Basically a clone of http://typedrummer.com/
+"""
+import os
+import sys
+import time
+import string
+import readline
+import threading
+from meep import open_output, NoteOn, NoteOff
+
+
+#drum_notes = {
+#    'a': 36,
+#    'b': 47,
+#    'c': 50,
+#}
+
+drum_notes = {}
+
+for note, char in enumerate(string.ascii_lowercase, start=35):
+    drum_notes[char] = note
+
+
+class Drummer:
+    def __init__(self, port, pattern=''):
+        self.port = port
+        self.pattern = pattern
+        self.sleep_time = 0.20
+
+        self._thread = threading.Thread(target=self._mainloop)
+        self._thread.daemon = True
+        self._thread.start()
+
+    def _mainloop(self):
+        while True:
+            for char in self.pattern:
+                note = drum_notes.get(char)
+                if note is not None:
+                    self.port.send(NoteOn(note, ch=10))
+                    self.port.send(NoteOff(note, ch=10))
+
+                time.sleep(self.sleep_time)
+
+
+gmsynth = open_output('GM')
+
+drummer = Drummer(gmsynth, pattern='b ')
+while True:
+    pattern = input().rstrip('\n')
+    drummer.pattern = pattern
