9ebc16500ba426a989d3a423956047b0d23d227e
Ole Martin Bjørndalen <ombdalen@gmail.com>
2024-05-02 08:01:33 +0200

    Added example_mpk_mini_print.py.

---
+++ example_mpk_mini_print.py
@@ -0,0 +1,9 @@
+from meep import open_input
+
+for msg in open_input('MPK'):
+    if msg.is_cc(1):
+        print('Filter knob:', msg.value)
+    elif msg.is_cc():
+        print('Other knob:', msg)
+    else:
+        print('Key?', msg)
