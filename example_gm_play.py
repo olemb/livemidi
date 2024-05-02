cbe14d0f88d731e9365b50e372c478d771d531b2
Ole Martin Bjørndalen <ombdalen@gmail.com>
2024-05-02 06:41:33 +0200

    Added example example_gm_play.py.

---
+++ example_gm_play.py
@@ -0,0 +1,12 @@
+import time
+from meep.messages import *
+from meep.sendmidi import open_output
+
+out = open_output('GM')
+
+for i in range(10):
+    note = i + 2
+    out.send(NoteOn())
+    time.sleep(0.3)
+    out.send(NoteOff())
+    time.sleep(0.1)
